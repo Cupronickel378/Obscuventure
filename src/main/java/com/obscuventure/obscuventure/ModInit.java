33b4f776a1ee0b2af6f5383fc69cd475d575a944
Cupronickel <genadobrohotov@gmail.com>
2026-06-29 11:55:38 +0300

    Add Creative Tab

--- src/main/java/com/obscuventure/obscuventure/ModInit.java
+++ src/main/java/com/obscuventure/obscuventure/ModInit.java
@@ -1,7 +1,11 @@
 package com.obscuventure.obscuventure;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -15,6 +19,7 @@ public class ModInit {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     public static final RegistryObject<Block> MYSTIC_BLOCK = BLOCKS.register("mystic_block",
             () -> new Block(BlockBehaviour.Properties.of()
@@ -34,8 +39,20 @@ public class ModInit {
     public static final RegistryObject<Item> DUNGEON_CORE_ITEM = ITEMS.register("dungeon_core",
             () -> new BlockItem(DUNGEON_CORE.get(), new Item.Properties()));
 
+    public static final RegistryObject<CreativeModeTab> OBSCUVENTURE_TAB = CREATIVE_MODE_TABS.register("obscuventure_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(MYSTIC_GEM.get()))
+                    .title(Component.translatable("creativetab.obscuventure"))
+                    .displayItems((parameters, output) -> {
+                        output.accept(MYSTIC_GEM.get());
+                        output.accept(MYSTIC_BLOCK.get());
+                        output.accept(DUNGEON_CORE_ITEM.get());
+                    })
+                    .build());
+
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
         ITEMS.register(eventBus);
+        CREATIVE_MODE_TABS.register(eventBus);
     }
 }
